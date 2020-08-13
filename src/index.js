edeac5da60362b5171d749b0df48419b224f46f0
Subham Ray <66999381+selfprogrammed@users.noreply.github.com>
2020-08-13 23:08:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,7 @@ import App from "./App";
 import firebase from "firebase";
 import "firebase/firestore";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyB96hDmgLK4C9Uys_mDvGoqiOaMVytS_rY",
-  authDomain: "evernote-clone-999.firebaseapp.com",
-  databaseURL: "https://evernote-clone-999.firebaseio.com",
-  projectId: "evernote-clone-999",
-  storageBucket: "evernote-clone-999.appspot.com",
-  messagingSenderId: "594625937859",
-  appId: "1:594625937859:web:0f9608ce21d42388befd76",
-  measurementId: "G-NPG77325N9",
-});
+//firebase.initializeApp(Firebase SDK Config (I have deleted from this file due to security reasons));
 
 ReactDOM.render(
   <React.StrictMode>
